890bde1bd96e339934a3eaf7de88bc637fcfa758
Han JU <ju.han.felix@gmail.com>
2012-08-18 15:42:42 +0200

    clean up

--- init.el
+++ init.el
@@ -1,19 +1,20 @@
-; My emacs init file
+;; My emacs init file
+;; JU Han
+;; ju.han.felix@gmail.com
 
-;; Load path
+;; Load path ----------------------------------------------------------
 (add-to-list 'load-path "~/.emacs.d")
 (progn (cd "~/.emacs.d")
         (normal-top-level-add-subdirs-to-load-path))
 (add-to-list 'load-path "~/.emacs.d/vendors")
 (progn (cd "~/.emacs.d/vendors")
         (normal-top-level-add-subdirs-to-load-path))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; User settings
+
+;; User settings ------------------------------------------------------
 (setq user-full-name "JU Han")
 (setq user-mail-address "ju.han.felix@gmail.com")
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Convenient set-ups
+;; Convenient set-ups -------------------------------------------------
 (show-paren-mode t)
 (setq show-paren-style 'parentheses)
 (mouse-avoidance-mode 'animate)
@@ -24,20 +25,19 @@
 (setq default-major-mode 'text-mode)
 (setq redisplay-dont-pause t)
 
-;; Line numbers
+;; Line numbers -------------------------------------------------------
 (require 'linum)
 (global-linum-mode t)
 
 
-;; Set default browser to Chromium
+;; Set default browser to Chromium ------------------------------------
 (setq browse-url-browser-function 'browse-url-generic
       browse-url-generic-program "chromium-browser")
 
 ;; (add-to-list 'default-frame-alist '(height . 42))
 ;; (add-to-list 'default-frame-alist '(width . 80))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Scheme
+;; Scheme -------------------------------------------------------------
 
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
@@ -59,8 +59,8 @@
    '(progn (define-key scheme-mode-map "\t" 'scheme-complete-or-indent)))
 (add-hook 'scheme-mode-hook 'lisp-coding-hook)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Color theme
+
+;; Color theme --------------------------------------------------------
 
 (require 'color-theme)
 (setq color-theme-is-global t)
@@ -79,8 +79,7 @@
  '(quack-pltish-keyword-face ((t (:foreground "darkturquoise" :weight bold))))
  '(quack-pltish-paren-face ((((class color) (background dark)) (:foreground "orange1")))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Configurations for LaTex
+;; Configurations for LaTex -------------------------------------------
 
 (load "auctex.el" nil t t)
 
@@ -106,12 +105,10 @@
 (add-hook 'LaTeX-mode-hook 'turn-on-auto-fill)
 (add-hook 'LaTeX-mode-hook (lambda () (abbrev-mode +1)))
 
-
-
 ;; (load "preview-latex.el" nil t t)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Configurations for Python
+
+;; Configurations for Python ------------------------------------------
 
 (require 'python-mode)
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
@@ -145,8 +142,8 @@
 ;; (define-key comint-mode-map [down] 'comint-next-matching-input-from-input)
 ;; (define-key comint-mode-map [up] 'comint-previous-matching-input-from-input)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;Autopair
+
+;; Autopair -----------------------------------------------------------
 
 (autoload 'autopair-global-mode "autopair" nil t)
 (autopair-global-mode t)
@@ -165,15 +162,16 @@
 ;;delete trailing spaces
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;Yasnippet
+
+;; Yasnippet ----------------------------------------------------------
+
 (require 'yasnippet-bundle)
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/snippets/")
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Auto complete
+;; Auto complete ------------------------------------------------------
+
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/vendors/auto-complete-1.3/dict")
 (ac-config-default)
@@ -193,15 +191,10 @@
 ;(add-hook 'LaTeX-mode-hook (lambda() (ac-latex-setup)))
 
 (require 'smart-operator)
-;;(require 'auto-complete-config)
-
-;; (add-hook 'after-init-hook 'session-initialize)
-;; (load "wcy-desktop")
-;; (wcy-desktop-init)
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ido-mode improve open file and change frame experience
+;; ido-mode ------------------------------------------------------------
+
 ;; (when (fboundp 'ido-mode)
 ;;   (ido-mode t)
 ;;   (setq ido-save-directory-list-file nil))
@@ -210,21 +203,21 @@
 (ido-mode 1)
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Javascript mode -- js2-mode
+;; Javascript mode -- js2-mode -----------------------------------------
+
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; C Programming Languauge
+;; C Programming Languauge ---------------------------------------------
+
 (require 'cc-mode)
 ;;(define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
 (define-key c-mode-map [return] 'newline-and-indent)
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Perl Programming
+;; Perl Programming ----------------------------------------------------
+
 ;; Use cperl-mode instead of the default perl-mode
 (add-to-list 'auto-mode-alist '("\\.\\([pP][Llm]\\|al\\)\\'" . cperl-mode))
 (add-to-list 'interpreter-mode-alist '("perl" . cperl-mode))
@@ -232,8 +225,8 @@
 (add-to-list 'interpreter-mode-alist '("miniperl" . cperl-mode))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Code folding
+;; Code folding --------------------------------------------------------
+
 (autoload 'hideshowvis-enable "hideshowvis" "Highlight foldable regions")
 (autoload 'hideshowvis-minor-mode
   "hideshowvis"
@@ -246,108 +239,8 @@
   (add-hook hook 'hideshowvis-enable))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Tarbar mode
 
-;; (require 'tabbar)
-;; (tabbar-mode 1)
-
-;; ;; * face
-;; (custom-set-variables
-;;  '(tabbar-separator (quote (0.5)))
-;;  '(tabbar-use-images nil))
-;; (defmacro tabbar-customize-faces-macro (foreground background sforeground uforeground ubackground)
-;; `(custom-set-faces
-;;  '(tabbar-button ((t (:inherit tabbar-default))))
-;;  '(tabbar-default ((((class color grayscale) (background dark)) (:inherit variable-pitch :background ,background :foreground ,foreground :height 0.8))))
-;;  '(tabbar-highlight ((t (:overline ,background))))
-;;  '(tabbar-selected ((t (:inherit tabbar-default :background "background-color-at-point" :foreground ,sforeground))))
-;;  '(tabbar-unselected ((t (:inherit tabbar-default :background ,ubackground :foreground ,uforeground)))))
-;; )
-
-
-;; (tabbar-customize-faces-macro
-;;  "gray50"                               ;foreground
-;;  "gray15"                               ;background
-;;  "yellow"                               ;sforeground
-;;  "gray60"                               ;uforeground
-;;  "gray25"                               ;ubackground
-;;  )
-
-;; ;; * keybind
-;; ;; (def-key-s 0
-;; ;;   "M-j" 'tabbar-backward-tab
-;; ;;   "M-k" 'tabbar-forward-tab
-
-;; ;;     )
-;; ;; * all tabs is just one group
-;; ;	(setq tabbar-buffer-groups-function
-;; ;	      (lambda ()
-;; ;	  	(list "All")))
-
-;; ;; * group rules
-;; (defun substring-buffer-name (m n &optional x)
-;;   "使用 substring 截取文件名时，在 buffer-name 后面加几个字符，
-;;    防止文件名过短引发错误"
-;;   (substring (concat
-;;               (if x
-;;                   (buffer-file-name)
-;;                 (buffer-name))
-;;               (make-string n ?*))
-;;              m n))
-
-;; (defun tabbar-buffer-groups ()
-;;   "Return the list of group names the current buffer belongs to.
-;; Return a list of one element based on major mode."
-;;   (list
-;;    (cond
-;; ;   ((or (get-buffer-process (current-buffer))
-;; ;        ;; Check if the major mode derives from `comint-mode' or
-;; ;        ;; `compilation-mode'.
-;; ;        (tabbar-buffer-mode-derived-p
-;; ;         major-mode '(comint-mode compilation-mode)))
-;; ;    "Process")
-
-;;     ((member (buffer-name)
-;;              '("*scratch*" "*Messages*"))
-;;      "Common")
-
-;;     ((memq major-mode
-;;            '(help-mode apropos-mode Info-mode Man-mode))
-;;      "Help")
-
-;;     ((eq major-mode 'dired-mode)
-;;      "Dired")
-
-;;     ((memq major-mode
-;;            '(rmail-mode
-;;              rmail-edit-mode vm-summary-mode vm-mode mail-mode
-;;              mh-letter-mode mh-show-mode mh-folder-mode
-;;              gnus-summary-mode message-mode gnus-group-mode
-;;              gnus-article-mode score-mode gnus-browse-killed-mode))
-;;      "Mail")
-
-;;     ((string-equal "*vc-" (substring-buffer-name 0 4))
-;;      "VC-mode Buffer")
-
-;;     ((string-equal "*sdcv*" (substring-buffer-name 0 6))
-;;      "User Buffer")
-
-;;     ((string-equal "*" (substring-buffer-name 0 1))
-;;      "Emacs Buffer")
-;; ;;;;
-;;     ((or (string-equal "emacs" (substring-buffer-name 1 6))
-;;          (string-equal init-dir (substring-buffer-name 0 (length init-dir) t)))
-;;      "Configuration")
-;; ;;;;
-;;     (t
-;;      "User Buffer")
-;;     )))
-
-;; (setq tabbar-buffer-groups-function 'tabbar-buffer-groups)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Eshell
+;; Eshell ---------------------------------------------------------------
 
 ;; Execute time for each command
 (add-hook 'eshell-load-hook
@@ -375,14 +268,16 @@
                    ;; ac-source-imenu
 ))
 
-;; ESS for R
+;; ESS for R ----------------------------------------------------------
+
 (add-to-list 'load-path "/usr/share/emacs/site-lisp/ESS/lisp/")
 (require 'ess-site)
 
-;; Minimap
+;; Minimap ------------------------------------------------------------
 (require 'minimap)
 
-;; Prediction Mode
+;; Prediction Mode ----------------------------------------------------
+
 ;; (add-to-list 'load-path "/home/darkjh/.emacs.d/vendors/predictive")
 ;; (add-to-list 'load-path "/homes/darkjh/.emacs.d/vendors/predictive/texinfo")
 ;; (add-to-list 'load-path "/home/darkjh/.emacs.d/vendors/predictive/html")
@@ -390,7 +285,8 @@
 ;; (require 'predictive)
 
 
-;; Ibus input mode
+;; Ibus input mode ----------------------------------------------------
+
 ;; http://www.emacswiki.org/emacs/IBusMode
 ;; 开启ibus 提供中文输入
 (require 'ibus)
@@ -412,7 +308,8 @@
 (global-set-key (kbd "s-SPC") 'ibus-toggle)
 
 
-;; Org-Mode set-up
+;; Org-Mode ------------------------------------------------------------
+
 (require 'org-install)
 (require 'org-publish)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
@@ -424,13 +321,16 @@
 	  (lambda () (setq truncate-lines nil)))
 (global-set-key "\C-cb" 'org-iswitchb)
 
-;; Word Count Mode
+
+;; Word Count Mode -----------------------------------------------------
 
 (autoload 'word-count-mode "word-count"
   "Minor mode to count words." t nil)
 (global-set-key "\M-+" 'word-count-mode)
 
-;; Octave Mode
+
+;; Octave Mode ---------------------------------------------------------
+
 (autoload 'octave-mode "octave-mod" nil t)
 (setq auto-mode-alist
 (cons '("\\.m$" . octave-mode) auto-mode-alist))
@@ -442,9 +342,28 @@
 (if (eq window-system 'x)
 (font-lock-mode 1))))
 
-;; Ruby Set-ups
+;; Ruby -----------------------------------------------------------------
+
 (autoload 'inf-ruby "inf-ruby" "Run an inferior Ruby process" t)
 (autoload 'inf-ruby-keys "inf-ruby" "" t)
 (eval-after-load 'ruby-mode
   '(add-hook 'ruby-mode-hook 'inf-ruby-keys))
 
+;; Markdown -------------------------------------------------------------
+
+(autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
+(setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
+(add-hook 'markdown-mode-hook
+	  '(lambda ()
+	     (auto-fill-mode 0)))
+
+;; Common Lisp ---------------------------------------------------------
+
+(setq inferior-lisp-program "/usr/bin/clisp")
+
+
+;; GDB -----------------------------------------------------------------
+(add-hook 'gdb-mode-hook '(lambda ()
+                            (define-key c-mode-base-map [(f5)] 'gud-go)
+                            (define-key c-mode-base-map [(f7)] 'gud-step)
+                            (define-key c-mode-base-map [(f8)] 'gud-next)))
